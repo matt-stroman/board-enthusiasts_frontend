2b6d49781abeaf05133b36991dda84dc8e17a81a
Matt Stroman <matt@mattstroman.com>
2026-04-09 05:49:10 -0500

    Fixing validation automation errors

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
+    testTimeout: 10000,
     environment: "jsdom",
     setupFiles: ["./src/test/setup.ts"],
     include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
